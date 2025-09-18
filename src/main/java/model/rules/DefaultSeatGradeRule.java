97b2595ef1642033a3b3303670965b64e4245702
ver <ver@sendy.ai>
2025-09-18 20:40:42 +0900

    fix: SeatNumber 의 row 는 Char

--- src/main/java/model/rules/DefaultSeatGradeRule.java
+++ src/main/java/model/rules/DefaultSeatGradeRule.java
@@ -9,11 +9,11 @@ import java.util.List;
 public class DefaultSeatGradeRule implements SeatGradeRule {
 
     public SeatGrade getSeatGrade(SeatNumber seatNumber) {
-        if (List.of("E", "F", "G", "H").contains(seatNumber.row())) {
+        if (List.of('E', 'F', 'G', 'H').contains(seatNumber.row())) {
             return SeatGrade.S;
         }
 
-        if (List.of("A", "B", "C", "D").contains(seatNumber.row())) {
+        if (List.of('A', 'B', 'C', 'D').contains(seatNumber.row())) {
             return SeatGrade.B;
         }
 
